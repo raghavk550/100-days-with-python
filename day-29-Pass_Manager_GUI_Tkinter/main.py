bc5677e5144b1b6f7f44f3176111be71fb363a3e
Raghav Kakria <raghav.kakria@appinventiv.com>
2026-07-15 11:26:07 +0530

    use pyperclip to copy str on clipboard

--- day-29-Pass_Manager_GUI_Tkinter/main.py
+++ day-29-Pass_Manager_GUI_Tkinter/main.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import messagebox
+import pyperclip
 
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -33,6 +34,7 @@ def generate_password():
     or
     """
     password = "".join(password_list)
+    pyperclip.copy(password)
 
     password_entry.insert(0, password)
 
